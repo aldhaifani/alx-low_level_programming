157d6889660bd53b1e4bda36f41afa2360f75528
Tareq <tareqaldhaifani@gmail.com>
2025-02-23 16:39:22 +0000

    Task 12 Software.. - refactored

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,24 +7,28 @@
  */
 int main(void)
 {
-	int i, j, k, l;
+	int i, j;
 
-	for (i = 0; i <= 9; i++)
-		for (j = 0; j <= 9; j++)
-			for (k = 0; k <= 9; k++)
-				for (l = j + 1; l <= 9 && (i * 10 + j) < (k * 10 + l); l++)
+	for (i = 0; i <= 98; i++)
+	{
+		for (j = 0; j <= 99; j++)
+		{
+			if (i < j)
+			{
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+				if (i != 98)
 				{
-					putchar(i + '0');
-					putchar(j + '0');
+					putchar(',');
 					putchar(' ');
-					putchar(k + '0');
-					putchar(l + '0');
-					if ((i * 10 + j) != 98)
-					{
-						putchar(',');
-						putchar(' ');
-					}
 				}
+			}
+		}
+	}
+
 	putchar('\n');
 
 	return (0);
