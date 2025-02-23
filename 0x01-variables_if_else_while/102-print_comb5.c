a6175eadb8850697815ab8711687bd6eb1a0e344
Tareq <tareqaldhaifani@gmail.com>
2025-02-23 16:12:56 +0000

    Task 12 Software.. - restructured the implementation for better accuracy

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,22 +10,19 @@ int main(void)
 	int i, j, k, l;
 
 	for (i = 0; i <= 9; i++)
-		for (j = 0; j <= 8; j++)
+		for (j = 0; j <= 9; j++)
 			for (k = 0; k <= 9; k++)
-				for (l = j + 1; l <= 9; l++)
+				for (l = j + 1; l <= 9 && (i * 10 + j) < (k * 10 + l); l++)
 				{
-					if (k <= l)
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(' ');
+					putchar(k + '0');
+					putchar(l + '0');
+					if ((i * 10 + j) != 98)
 					{
-						putchar(i + '0');
-						putchar(j + '0');
+						putchar(',');
 						putchar(' ');
-						putchar(k + '0');
-						putchar(l + '0');
-						if (!(i == 9 && j == 8 && k == 9 && l == 9))
-						{
-							putchar(',');
-							putchar(' ');
-						}
 					}
 				}
 	putchar('\n');
