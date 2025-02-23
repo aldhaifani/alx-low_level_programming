5c82ec133ceaf44e6682474f960cf7f9a423b11a
Tareq <tareqaldhaifani@gmail.com>
2025-02-23 12:32:20 +0000

    Task 8 UNIX is... - removed extra libs

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <unistd.h>
-#include <string.h>
 
 /**
  * main - Entry point of the program
@@ -10,7 +9,7 @@ int main(void)
 {
 	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(1, s, strlen(s));
+	write(1, s, 59);
 
 	return (0);
 }
