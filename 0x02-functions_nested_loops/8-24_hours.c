3085c548351e8e68670992e31deaeea2bb00634f
Tareq <tareqaldhaifani@gmail.com>
2025-02-26 08:47:03 +0000

    Task 8 I'm.. - went back to _putchar

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * jack_bauer - function that prints every minute of the day of Jack Bauer,
@@ -10,16 +10,32 @@ void jack_bauer(void)
 {
 	int h, m;
 
-	for (h = 0; h <= 23; h++)
+	for (h = 0; h < 24; h++)
 	{
-		for (m = 0; m <= 59; m++)
+		for (m = 0; m < 60; m++)
 		{
 			if (h < 10)
-				printf("0");
-			printf("%d:", h);
+			{
+				_putchar('0');
+				_putchar(h + '0');
+			}
+			else
+			{
+				_putchar((h / 10) + '0');
+				_putchar((h % 10) + '0');
+			}
+			_putchar(':');
 			if (m < 10)
-				printf("0");
-			printf("%d\n", m);
+			{
+				_putchar('0');
+				_putchar(m + '0');
+			}
+			else
+			{
+				_putchar((m / 10) + '0');
+				_putchar((m % 10) + '0');
+			}
+			_putchar('\n');
 		}
 	}
 }
